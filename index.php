d1a14f8291791d44a05a4f135c51b1f40e221ca5
white developer <52850655+mrwhitedeveloper@users.noreply.github.com>
2021-09-06 18:48:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,34 +3,86 @@ include 'HtmlToDoc.class.php';
 
 if (isset($_REQUEST['frm'])) {
     $htmlToDoc = new HtmlToDoc();
-    // convert html from url
-    // $htmlToDoc->createDocFromURL( "http://google.com", "output", true );
-    // or from hdd
-    $htmlToDoc->createDocFrom($_REQUEST['content'], "a.doc", true);
+    $htmlToDoc->createDocFrom($_REQUEST['content'], "html.doc", true);
 }
 
 if (isset($_REQUEST['frm2'])) {
     $htmlToDoc = new HtmlToDoc();
-    // convert html from url
-    // $htmlToDoc->createDocFromURL( "http://google.com", "output", true );
-    // or from hdd
-    $htmlToDoc->createDoc($_REQUEST['fname'], "a.doc", true);
+    $htmlToDoc->createDoc($_REQUEST['fname'], "html.doc", true);
+}
+if (isset($_REQUEST['frm2'])) {
+    $htmlToDoc = new HtmlToDoc();
+     $htmlToDoc->createDocFromURL($_REQUEST['fname'], "html.doc", true );
 }
 ?>
+<html>
 
-<form method="POST" action="">
-    <input type="hidden" name="frm" />
-    <label for="fname">Content:</label><br>
-    <textarea type="text" id="content" name="content">
-</textarea>
+<head>
+    <title>Convert Html To Doc</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</head>
+</body>
+<div class="container">
+    <div class="row mt-2">
+        <div class="col-md-12">
+            <form method="POST" action="">
+                <div class="form-group">
+                    <input type="hidden" name="frm" />
+                    <label for="fname">Content:</label><br>
+                    <textarea class="form-control" type="text" id="content" name="content" rows="10"
+                        placeholder="Html Content "></textarea>
 
-    <input type="submit" value="Submit">
-</form>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <form method="POST" action="">
+                <div class="form-group">
+                    <input type="hidden" name="frm2" />
+                    <label for="fname">File Content:</label><br>
+                    <input class="form-control" type="text" name="fname" placeholder="Html file name " />
 
-<form method="POST" action="">
-    <input type="hidden" name="frm2" />
-    <label for="fname">File Content:</label><br>
-    <input type="text" name="fname" />
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <form method="POST" action="">
+                <div class="form-group">
+                    <input type="hidden" name="frm3" />
+                    <label for="fname">File Url:</label><br>
+                    <input class="form-control" type="url" name="fname" placeholder="Html file Url " />
 
-    <input type="submit" value="Submit">
-</form>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</div>
+</body>
+
+</html>
